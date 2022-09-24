491d175389373d2793aaedbffb44a90ef87916b2
dannylu03 <danny.lu075@gmail.com>
2022-09-24 17:14:37 -0400

    Fixed task 3

--- src/main/java/Basics.java
+++ src/main/java/Basics.java
@@ -104,7 +104,7 @@ public class Basics {
          */
 
         for (int i = 10; i >= 0; i--) {
-            System.out.println(i);
+            System.out.println("Current count: " + i);
         }
     }
 
